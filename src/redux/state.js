5309de1f2f78d8d5f470cc56da70f22831cc9e95
Hari Om Gupta <hariomgupta0303@gmail.com>
2025-10-28 07:30:28 +0530

    done fetch listing by category

--- src/redux/state.js
+++ src/redux/state.js
@@ -16,10 +16,14 @@ export const userSlice = createSlice({
         setLogout: (state) =>{
             state.user = null
             state.token = null
+        },
+        setListings:(state, action)=>{
+            state.listings = action.payload.listings
         }
     }
 })
 
 export const {setLogin} = userSlice.actions
 export const {setLogout} = userSlice.actions
+export const {setListings} = userSlice.actions
 export default userSlice.reducer
